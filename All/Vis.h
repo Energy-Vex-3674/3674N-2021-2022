5e389ea3010ad8405a80031cf63c9bf18bf5cebe
KyleSpiller <85643781+KyleSpiller@users.noreply.github.com>
2022-03-01 09:18:17 -0600

    Files for all projects were added

---
+++ All/Vis.h
@@ -0,0 +1,11 @@
+/*vex-vision-config:begin*/
+#include "vex.h"
+vex::vision::signature RING = vex::vision::signature (1, 183, 1659, 921, 2473, 7909, 5191, 1.3, 0);
+vex::vision::signature SIG_2 = vex::vision::signature (2, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_3 = vex::vision::signature (3, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_4 = vex::vision::signature (4, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_5 = vex::vision::signature (5, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_6 = vex::vision::signature (6, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_7 = vex::vision::signature (7, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision Vis = vex::vision (vex::PORT15, 61, RING, SIG_2, SIG_3, SIG_4, SIG_5, SIG_6, SIG_7);
+/*vex-vision-config:end*/
